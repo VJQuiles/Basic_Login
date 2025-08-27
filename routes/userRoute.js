d421a9216aaa9397b579272a76447054a4c6ecad
Vince Quiles <vjquiles@msn.com>
2025-08-27 10:50:28 -0400

    Simple routes written, functionality will be handled in controllers.

---
+++ routes/userRoute.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/register', 'controller goes here')
+
+router.post('/login', 'contoller goes here')
+
+module.exports = router
