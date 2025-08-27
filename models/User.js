86f81f0668ca2710bb61977fe979e1b7324def30
Vince Quiles <vjquiles@msn.com>
2025-08-27 10:16:02 -0400

    User model with password creation and password check middleware placed in. Now i can set up the routes and then handle functionality in the controller

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+const bcrypt = require('bcrypt')
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@+\.+/, "Must match an email address!"]
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    }
+})
+
+//Pre-save middleware to create a password
+userSchema.pre("save", async function (next) {
+    if (this.isNew || this.isModified("password")) {
+        const saltRounds = 10
+        this.password = await bcrypt.hash(this.password, saltRounds)
+        console.log(this.password)
+    }
+    next()
+})
+
+//Password check
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password)
+}
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
